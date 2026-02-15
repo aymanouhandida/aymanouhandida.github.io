9431427210cfcf6634f8c2bba3c5b8979d6169ee
OussamaELKASMI <m.elkasmioussama@gmail.com>
2026-02-15 22:47:07 +0000

    Done

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: '//aymanouhandida/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
